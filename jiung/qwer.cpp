4db86bc8bef2ebb6884e9d53b4c155ec5447799c
hanjiung <smert2580@naver.com>
2021-01-29 17:54:07 +0900

    fix gitignore

---
+++ jiung/qwer.cpp
@@ -0,0 +1,13 @@
+/******
+ * Author      :    Jiung
+ * Filename    :    qwer.cpp
+ * Version     :    Apple clang version 12.0.0 (clang-1200.0.32.27)
+ * Date        :    2021-01-29
+ * Copyright   :    Free
+ */
+  
+#include <iostream>
+  
+using namespace std;
+  
+tst
